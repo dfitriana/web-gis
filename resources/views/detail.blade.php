8fe1bbcfa05f149b659e1a54840ca04e8e588e19
dfitriana <diyahayufa@students.unnes.ac.id>
2022-12-11 02:00:05 +0700

    Update detail.blade.php

--- resources/views/detail.blade.php
+++ resources/views/detail.blade.php
@@ -13,24 +13,67 @@
     </style>
 </head>
 <body>
-    <div id="map"></div>
+    <div id="fly">
+
+        <div id="map" class="w-[65%] h-[100vh]"></div>
+    </div>
     <script>
         mapboxgl.accessToken = 'pk.eyJ1IjoiZGZpdHJpYW5hIiwiYSI6ImNsYmc0a3pwejA1dHczcHEzYmJpcG5ndXYifQ.I4HBCvvZH5hHK8zC0-nFTA';
+        // These options control the camera position after animation
+        const start = {
+            center: [{{ $pariwisata->longitude}},{{ $pariwisata->latitude}}],
+            zoom: 2,
+            pitch: 0,
+            bearing: 0
+        };
+        const end = {
+            center: [{{ $pariwisata->longitude}},{{ $pariwisata->latitude}}],
+            zoom: 12.5,
+            bearing: 50,  //animasi seperti berputar
+            pitch: 75     //peta datar
+        };
         const map = new mapboxgl.Map({
             container: 'map',
-            // Choose from Mapbox's core styles, or make your own style with Mapbox Studio
             style: 'mapbox://styles/mapbox/streets-v12',
             center: [{{ $pariwisata->longitude}},{{ $pariwisata->latitude}}],
-            zoom: 12
+            // zoom: 12
+            ...start
         });
+
+        const popUp = new mapboxgl.Popup({
+            offset:35,
+            closeOnClick: false,
+            closeButton: false,
+        })
+        .setLngLat([{{ $pariwisata->longitude}},{{ $pariwisata->latitude}}])
+        .setHTML('<div class="text-xs text-green-c1">{{ $pariwisata->places}}</div>')
+        .setMaxWidth("400px")
+        .addTo(map);
     
         // Create a default Marker and add it to the map.
         const marker1 = new mapboxgl.Marker()
         .setLngLat([{{ $pariwisata->longitude}},{{ $pariwisata->latitude}}])
         .addTo(map);
+        
+        // add navigation control
         map.addControl(new mapboxgl.NavigationControl());
         
-        // console.log(map);
+        // setting fly animation
+        let isAtStart = true;
+        document.getElementById('fly').addEventListener('click', () => {
+            // depending on whether we're currently at point a or b,
+            // aim for point a or b
+            const target = isAtStart ? end : start;
+            isAtStart = !isAtStart;
+            
+            map.flyTo({
+                ...target, // Fly to the selected target
+                duration: 12000, // Animate over 12 seconds
+                essential: true // This animation is considered essential with
+                //respect to prefers-reduced-motion
+            });
+        });
+        
     </script>
 </body>
 </html>
