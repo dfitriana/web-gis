d0bb300f5150a7bc7e5e9ce8166c8170236cab3c
dfitriana <diyahayufa@students.unnes.ac.id>
2022-12-11 21:50:55 +0700

    Update index.blade.php

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -30,7 +30,7 @@
               <a href="/login">
                 <button type="submit" class="border flex px-3 py-1 gap-2 rounded-md hover:shadow-c7">
                   <div class="pt-[3px]">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-log-out"><path d="M9 21H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h4"></path><polyline points="16 17 21 12 16 7"></polyline><line x1="21" y1="12" x2="9" y2="12"></line></svg>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-log-in"><path d="M15 3h4a2 2 0 0 1 2 2v14a2 2 0 0 1-2 2h-4"></path><polyline points="10 17 15 12 10 7"></polyline><line x1="15" y1="12" x2="3" y2="12"></line></svg>
                   </div>
                   <div class="text-sm font-normal">Login</div>
                 </button>
